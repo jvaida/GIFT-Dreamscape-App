1e936e307858f8f7a6996cf76964a67725ac6c8c
Austin Porter <aportercoder@gmail.com>
2022-07-23 23:32:58 -0700

    Updated experience scene
    New experience scene with scene switching, global offset, and more

---
+++ unity-template/Assets/_DSFramework/DSFSceneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Artanim;
+using UnityEngine;
+
+public class DSFSceneController : MonoBehaviour
+{
+    public void LoadGameSceneFadeToBlack( string sceneName )
+    {
+        LoadGameScene(sceneName, Artanim.Location.Messages.Transition.FadeBlack);
+    }
+
+    public void LoadGameSceneFadeToWhite(string sceneName)
+    {
+        LoadGameScene(sceneName, Artanim.Location.Messages.Transition.FadeWhite);
+    }
+
+    public void LoadGameScene(string sceneName)
+    {
+        LoadGameScene(sceneName, Artanim.Location.Messages.Transition.None);
+    }
+
+    private void LoadGameScene( string sceneName, Artanim.Location.Messages.Transition transition )
+    {
+        //make sure there is a GameController in the scene before attempting to load
+        GameController _gameController = FindObjectOfType<GameController>();
+        if ( _gameController != null )
+        {
+            _gameController.LoadGameScene(sceneName, transition); //Call the GameController function with specified transition type
+        } else
+        {
+            Debug.LogError("Could not find a GameController in the scene while trying to switch scenes.");
+        }
+    }
+}
