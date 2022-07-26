264a2df4bc649712e8a1debb5ba673bd3886e2e1
Dion Pimentel <dionpimentel260@gmail.com>
2022-07-25 19:58:19 -0700

    Added Trackable and interactions to space scene

---
+++ unity-template/Assets/_DSFramework/Scripts/MaterialSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSwitch : MonoBehaviour
+{
+    MeshRenderer mesh;
+    [SerializeField]
+    private Material switchMat;
+
+    Material originalMat;
+    bool hit;
+
+    void Awake()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        originalMat = mesh.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(hit) return;
+        //mesh.material = originalMat;
+    }
+
+    void Change()
+    {
+        Material currentMat = mesh.material;
+        if(currentMat == originalMat)
+        {
+            mesh.material = switchMat;
+        }
+        else
+        {
+            mesh.material = originalMat;
+        }
+
+        //hit = true;
+    }
+
+
+}
---
+++ unity-template/Assets/_DSFramework/Scripts/RaycastRod.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastRod : MonoBehaviour
+{
+    bool hitting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hitting = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            Debug.Log("hit in change layer");
+            if (!hitting)
+            {
+                MaterialSwitch script = hit.collider.gameObject.GetComponent<MaterialSwitch>();
+                if (script)
+                {
+                    Debug.Log("has desired script");
+                    hit.transform.SendMessage("Change");
+                }
+                hitting = true;
+            }
+
+        }
+        else
+        {
+            hitting = false;
+        }
+    }
+}
