6bfb53da2d347aef5d230fe848ce1c6b4f3a15d1
msrivas-7 <msrivas7@asu.edu>
2022-08-01 16:28:01 -0700

    Haptics Update - Fix 2

---
+++ unity-template/Assets/HapticFloorController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using System;
+using Artanim.Utils;
+using System.Collections.Generic;
+using System.Collections;
+using Dreamscape;
+
+
+public class HapticFloorController : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(VibrateAll());   
+    }
+
+    private IEnumerator VibrateAll()
+    {
+        HapticAudio_main main = null;
+
+        while (!main)
+        {
+            main = HapticAudio_main.Instance;
+            yield return new WaitForEndOfFrame();
+        }
+        FloorBlinker blinker = FindObjectOfType<FloorBlinker>();
+
+        if (blinker)
+            blinker.BlinkFloor();
+
+        //turn all panels on
+        foreach (HapticAudio_main.AudioChannel ac in main.AudioChannelsConfiguration.AudioChannels)
+        {
+            Debug.Log("setting channels to zero gain" + ac.channelName_1 + ac.channelName_2);
+            //stereo gain two per each panel
+            ac.gain_1 = 1.0f;
+            ac.gain_2 = 1.0f;
+        }
+
+
+    }
+}
